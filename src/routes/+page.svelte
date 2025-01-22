968d6f4431196e7fad61f90a8b8d7434c12c3bf6
kuchromik <kai.chromik@online.de>
2025-01-22 13:08:24 +0100

    Implement user sign-in and sign-out functionality in SvelteKit app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <script>
     import { app, auth } from '$lib/FireBase.js';
     import { getFirestore, collection, getDocs } from 'firebase/firestore';
-    import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+    import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
     import { onMount } from 'svelte';
 
     let email = '';
     let password = '';
+    let user = $state(null);
 
     const db = getFirestore(app);
     const auth2 = getAuth();
+    
 
     async function handleSignIn() {
         try {
             const userCredential = await signInWithEmailAndPassword(auth2, email, password);
-            const user = userCredential.user;
+            user = userCredential.user;
             console.log(user);
             getCustomersFromCollection();
         } catch (error) {
@@ -27,10 +29,22 @@
             console.log(`${doc.id} => ${doc.data()}`);
         });
     }
+
+    signOut(auth2).then(() => {
+    // Sign-out successful.
+    user = null;
+    console.log('Sign-out successful');
+    }).catch((error) => {
+    // An error happened.
+    });
 </script>
 <h1>Welcome to UTB2026</h1>
-<input type="email" bind:value={email} placeholder="Email" />
-<input type="password" bind:value={password} placeholder="Password" />
-<button on:click={handleSignIn}>Sign In</button>
+{#if !(user)}
+    <input type="email" bind:value={email} placeholder="Email" />
+    <input type="password" bind:value={password} placeholder="Password" />
+    <button onclick={handleSignIn}>Sign In</button>
+{:else}
+    <button onclick={()=> signOut(auth2)}>Sign Out</button>
+{/if}
 
 
