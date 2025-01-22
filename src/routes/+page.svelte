e7ed29e8c80be6936398f88f077e263a5fcdcda7
kuchromik <kai.chromik@online.de>
2025-01-22 12:32:27 +0100

    Add Firebase authentication and Firestore integration to the SvelteKit app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { app, auth } from '$lib/FireBase.js';
+    import { getFirestore, collection, getDocs } from 'firebase/firestore';
+    import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+    import { onMount } from 'svelte';
+
+    let email = '';
+    let password = '';
+
+    const db = getFirestore(app);
+    const auth2 = getAuth();
+
+    async function handleSignIn() {
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth2, email, password);
+            const user = userCredential.user;
+            console.log(user);
+            getCustomersFromCollection();
+        } catch (error) {
+            console.error(error.code, error.message);
+        }
+    }
+
+    async function getCustomersFromCollection() {
+        const querySnapshot = await getDocs(collection(db, "customer"));
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+        });
+    }
+</script>
+<h1>Welcome to UTB2026</h1>
+<input type="email" bind:value={email} placeholder="Email" />
+<input type="password" bind:value={password} placeholder="Password" />
+<button on:click={handleSignIn}>Sign In</button>
+
+
