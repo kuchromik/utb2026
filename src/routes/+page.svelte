6e8f62896dd743093e598e8d542ae8ab2f484fc6
Kai-Uwe Chromik <kai.chromik@online.de>
2025-01-27 08:17:14 +0100

    Refactor job readiness toggles and enhance job display in SvelteKit app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,21 +42,49 @@
     }   
     */
     async function getJobsFromCollection() {
+        jobs = [];
         const q = query(collection(db, "Jobs"), where("archiv", "==", false));
         const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
-            jobs = [...jobs, doc.data()];
-            jobs.sort((a, b) => (b.jobstart) - (a.jobstart));
+            let ID = doc.id;
+            let job = { id: ID, ...doc.data() };
+            jobs = [...jobs, job];
         });
+        jobs.sort((a, b) => (b.jobstart) - (a.jobstart));
     }
 
-    async function togglePaperReady(job) {
-        const jobRef = doc(db, "Jobs", job.id);
-        await updateDoc(jobRef, {
-            paper_ready: !job.paper_ready
-        });
-        $inspect(job);
+    async function toggleSomethingIsReady(whatsIsReady, ID) {
+        const jobRef = doc(db, "Jobs", ID);
+        if (whatsIsReady === "paper") {
+            await updateDoc(jobRef, {
+                paper_ready: !ID.paper_ready
+            });
+            console.log(whatsIsReady, "status geändert on Firestore");
+        } else if (whatsIsReady === "plates") {
+            await updateDoc(jobRef, {
+                plates_ready: !ID.plates_ready
+            });
+            console.log(whatsIsReady, "status geändert on Firestore");
+        }
+        else if (whatsIsReady === "print") {
+            await updateDoc(jobRef, {
+                print_ready: !ID.print_ready
+            });
+            console.log(whatsIsReady, "status geändert on Firestore");
+        }
+        else if (whatsIsReady === "invoice") {
+            await updateDoc(jobRef, {
+                invoice_ready: !ID.invoice_ready
+            });
+            console.log(whatsIsReady, "status geändert on Firestore");
+        }
+        else if (whatsIsReady === "payed") {
+            await updateDoc(jobRef, {
+                payed: !ID.payed
+            });
+            console.log(whatsIsReady, "status geändert on Firestore");
+        }
+        
     }
 
  
@@ -87,18 +115,31 @@
         <li>
             <div class="joblist">
                 <div class="jobstart">
-                    <p>{new Date(job.jobstart * 1000).toLocaleString('de-DE', { dateStyle: 'short', timeStyle: 'short' })}</p>
+                    <small>{new Date(job.jobstart * 1000).toLocaleString('de-DE', { dateStyle: 'short', timeStyle: 'short' })}</small>
                 </div>
-                <div class="jobID"><p>{job.id}</p></div>
+                <!--<div class="jobID"><small>{job.id}</small></div>-->
                 <div class="customer"><p>{job.customer}</p></div>
                 <div class="jobname"><p>{job.jobname}</p></div>
                 <div class="quantity"><p>{job.quantity}</p></div>
                 <div class="details"><p>{job.details}</p></div>
                 <div class="amount"><p>{job.amount}</p></div>
                 <div class="producer"><p>{job.producer}</p></div>
-                <div class="paper-ready">
-                    <label><input type="checkbox" name="Papier?" bind:checked={job.paper_ready} disabled={!(job.producer === 'chr' || job.producer === 'doe')} onchange={() => togglePaperReady(job)}/>Papier?</label>
+                <div class="ready">
+                    <label>Papier?<input type="checkbox" name="Papier?" bind:checked={job.paper_ready} disabled={!(job.producer === 'chr' || job.producer === 'doe')} onchange={() => toggleSomethingIsReady("paper", job.id)}/></label>
                 </div>
+                <div class="ready">
+                    <label>Platten?<input type="checkbox" name="Platten?" bind:checked={job.plates_ready} disabled={!(job.producer === 'chr')} onchange={() => toggleSomethingIsReady("plates", job.id)}/></label>
+                </div>
+                <div class="ready">
+                    <label>Druck?<input type="checkbox" name="Druck?" bind:checked={job.print_ready} disabled={!(job.producer === 'chr' || job.producer === 'doe')} onchange={() => toggleSomethingIsReady("print", job.id)}/></label>
+                </div>
+                <div class="ready">
+                    <label>Rechnung?<input type="checkbox" name="Platten?" bind:checked={job.invoice_ready} disabled={!(true)} onchange={() => toggleSomethingIsReady("invoice", job.id)}/></label>
+                </div>
+                <div class="ready">
+                    <label>Zahlung?<input type="checkbox" name="Zahlung?" bind:checked={job.payed_ready} disabled={!(true)} onchange={() => toggleSomethingIsReady("payed", job.id)}/></label>
+                </div>
+                
             </div>
         </li>
     {/each}
@@ -123,6 +164,12 @@
 -->
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    * {
+        font-family: 'Roboto', sans-serif;
+        font-size: 12px;
+    }
     li {
         list-style-type: none;
     }
@@ -160,7 +207,7 @@
     .producer {
         flex: 1;
     }
-    .paper-ready {
+    .ready {
         flex: 1;
     }
 </style>
