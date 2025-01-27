2b69ebfad2e974ad6e9206a25683831960d7972d
Kai-Uwe Chromik <kai.chromik@online.de>
2025-01-27 20:58:56 +0100

    Enhance user authentication and job display functionality in SvelteKit app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import { app, auth } from '$lib/FireBase.js';
     import { getFirestore, collection, getDocs, onSnapshot, query, where, doc, updateDoc } from 'firebase/firestore';
     import { signInWithEmailAndPassword, signOut } from "firebase/auth";
-    import { onMount } from 'svelte';
 
     let email = $state('');
     let password = $state('');
@@ -12,13 +11,16 @@
     let selectedCustomer = $state('');
     
     const db = getFirestore(app);
+
+    let user = $state();
     
 
     async function handleSignIn() {
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
+            user = userCredential.user.email;
             loggedIn = true;
+            getJobsFromCollection();
         } catch (error) {
             console.error(error.code, error.message);
         }
@@ -29,6 +31,9 @@
             await signOut(auth);
             console.log("User signed out");
             loggedIn = false;
+            jobs = [];
+            email = '';
+            password = '';
         } catch (error) {
             console.error(error.code, error.message);
         }
@@ -40,19 +45,6 @@
             customers = [...customers, doc.data()];
         });
     }   
-    */
-   /*
-    async function getJobsFromCollection() {
-        jobs = [];
-        const q = query(collection(db, "Jobs"), where("archiv", "==", false));
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            let ID = doc.id;
-            let job = { id: ID, ...doc.data() };
-            jobs = [...jobs, job];
-        });
-        jobs.sort((a, b) => (b.jobstart) - (a.jobstart));
-    }
     */
     async function getJobsFromCollection() {
         jobs = [];
@@ -66,100 +58,116 @@
             });
             jobs.sort((a, b) => (b.jobstart) - (a.jobstart));
         });
+        
     }
+   
 
-    async function toggleSomethingIsReady(whatsIsReady, ID) {
+    async function toggleSomethingIsReady(whatsIsReady, ID, isReady) {
         const jobRef = doc(db, "Jobs", ID);
         if (whatsIsReady === "paper") {
             await updateDoc(jobRef, {
-                paper_ready: !(ID.paper_ready)
+                paper_ready: !(isReady)
             });
             console.log(whatsIsReady, "status geändert on Firestore");
         } else if (whatsIsReady === "plates") {
             await updateDoc(jobRef, {
-                plates_ready: !(ID.plates_ready)
+                plates_ready: !(isReady)
             });
             console.log(whatsIsReady, "status geändert on Firestore");
         }
         else if (whatsIsReady === "print") {
             await updateDoc(jobRef, {
-                print_ready: !(ID.print_ready)
+                print_ready: !(isReady)
             });
             console.log(whatsIsReady, "status geändert on Firestore");
         }
         else if (whatsIsReady === "invoice") {
-            console.log(ID.invoice_ready);
             await updateDoc(jobRef, {
-                invoice_ready: !(ID.invoice_ready)
+                invoice_ready: !(isReady)
             });
             console.log(whatsIsReady, "status geändert on Firestore");
         }
         else if (whatsIsReady === "payed") {
             await updateDoc(jobRef, {
-                payed: !(ID.payed)
+                payed_ready: !(isReady)
             });
             console.log(whatsIsReady, "status geändert on Firestore");
         }
         
     }
 
- 
-
 </script>
-<h1>Welcome to UTB2026</h1>
 
-{#if loggedIn}
-    <h2>User is logged in</h2>
-{:else}
-    <h2>User is not logged in</h2>
-{/if}
+<main>
 
+<h1 class="headline">Welcome to UTB2026</h1>
 
-
-<input type="email" bind:value={email} placeholder="Email" />
-<input type="password" bind:value={password} placeholder="Password" />
-<button onclick={handleSignIn}>Sign In</button>
-<hr>
-<button onclick={()=> handleLogOut()}>Sign Out</button>
-<hr>
-<button onclick={()=> getCustomersFromCollection()}>Get Customers, inaktiv!</button>
-<hr>
-<button onclick={()=> getJobsFromCollection()}>Get Jobs</button>
+<div class = "login">
+    {#if !loggedIn}
+        <input type="email" bind:value={email} placeholder="Email" />
+        <input type="password" bind:value={password} placeholder="Password" />
+   
+        <button onclick={handleSignIn}>Sign In</button>
+    {:else}
+        <button onclick={()=> handleLogOut()}>Sign Out</button>
+    {/if}
+    <div>
+        {#if loggedIn}
+            <h2 style="color: darkgreen;">Logged as {user}</h2>
+        {:else}
+            <h2 style="color: lightcoral;">No User logged</h2>
+        {/if}
+    </div>
+</div>
 <hr>
 <ul>
-    {#each jobs as job}
+    {#each jobs as job, index}
         <li>
-            <div class="joblist">
+            <div class="joblist {index % 2 === 0 ? 'secondRow' : ''}">
                 <div class="jobstart">
-                    <small>{new Date(job.jobstart * 1000).toLocaleString('de-DE', { dateStyle: 'short', timeStyle: 'short' })}</small>
+                    <p>{new Date(job.jobstart * 1000).toLocaleString('de-DE', { dateStyle: 'short', timeStyle: 'short' })}</p>
                 </div>
-                <div class="jobID"><small>{job.id}</small></div>
-                <div class="customer"><p>{job.customer}</p></div>
+                <!--<div class="jobID"><p>{job.id}</p></div>-->
+                <div class="customer"><p><strong>{job.customer}</strong></p></div>
                 <div class="jobname"><p>{job.jobname}</p></div>
-                <div class="quantity"><p>{job.quantity}</p></div>
+                <div class="quantity"><p><strong>{job.quantity}</strong></p></div>
                 <div class="details"><p>{job.details}</p></div>
-                <div class="amount"><p>{job.amount}</p></div>
+                <div class="amount"><p><strong>{job.amount}</strong></p></div>
                 <div class="producer"><p>{job.producer}</p></div>
                 <div class="ready">
-                    <label>Papier?<input type="checkbox" name="Papier?" bind:checked={job.paper_ready} disabled={!(job.producer === 'chr' || job.producer === 'doe')} onclick={() => toggleSomethingIsReady("paper", job.id)}/></label>
+                    {#if (job.producer === 'chr' || job.producer === 'doe')}
+                        <label>Papier?<input type="checkbox" name="Papier?" bind:checked={job.paper_ready} onclick={() => toggleSomethingIsReady("paper", job.id, job.paper_ready)}/></label>
+                    {:else}
+                        <input type="hidden" name="Papier?"/>
+                    {/if}
                 </div>
                 <div class="ready">
-                    <label>Platten?<input type="checkbox" name="Platten?" bind:checked={job.plates_ready} disabled={!(job.producer === 'chr')} onclick={() => toggleSomethingIsReady("plates", job.id)}/></label>
+                    {#if (job.producer === 'chr')}
+                        <label>Platten?<input type="checkbox" name="Platten?" bind:checked={job.plates_ready} onclick={() => toggleSomethingIsReady("plates", job.id, job.plates_ready)}/></label>
+                    {:else}
+                        <input type="hidden" name="Platten?"/>
+                    {/if}
+                </div>
+
+                <div class="ready">
+                    {#if (job.producer === 'chr' || job.producer === 'doe')}
+                        <label>Druck?<input type="checkbox" name="Druck?" bind:checked={job.print_ready} onclick={() => toggleSomethingIsReady("print", job.id, job.print_ready)}/></label>
+                    {:else}
+                        <input type="hidden" name="Druck?"/>
+                    {/if}
                 </div>
                 <div class="ready">
-                    <label>Druck?<input type="checkbox" name="Druck?" bind:checked={job.print_ready} disabled={!(job.producer === 'chr' || job.producer === 'doe')} onclick={() => toggleSomethingIsReady("print", job.id)}/></label>
+                    <label>Rechnung?<input type="checkbox" name="Platten?" bind:checked={job.invoice_ready} onclick={() => toggleSomethingIsReady("invoice", job.id, job.invoice_ready)}/></label>
                 </div>
                 <div class="ready">
-                    <label>Rechnung?<input type="checkbox" name="Platten?" bind:checked={job.invoice_ready} disabled={!(true)} onclick={() => toggleSomethingIsReady("invoice", job.id)}/></label>
-                </div>
-                <div class="ready">
-                    <label>Zahlung?<input type="checkbox" name="Zahlung?" bind:checked={job.payed_ready} disabled={!(true)} onclick={() => toggleSomethingIsReady("payed", job.id)}/></label>
+                    <label>Zahlung?<input type="checkbox" name="Zahlung?" bind:checked={job.payed_ready} onclick={() => toggleSomethingIsReady("payed", job.id, job.payed_ready)}/></label>
                 </div>
                 
             </div>
         </li>
     {/each}
 </ul>
+</main>
 <!--
 <ul>
     {#each customers as customer}
@@ -186,18 +194,52 @@
         font-family: 'Roboto', sans-serif;
         font-size: 12px;
     }
+
+    main {
+        max-width: 1600px;
+        margin: 0 auto;
+    }
+
+    .headline {
+        font-size: 24px;
+        font-weight: 700;
+        margin-bottom: 20px;
+        text-align: center;
+    }
+
+
+    ul {
+        padding-inline-start: 0;
+    }
+
     li {
         list-style-type: none;
     }
+
+    .login {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+        margin-bottom: 20px;
+    }
+
 
     .joblist {
         display: flex;
         align-items: center;
         gap: 10px;
-        border: 1px solid black;
+        background-color: whitesmoke;
+        border: 1px solid grey;
+        border-radius: 5px;
         padding: 10px;
-        margin: 5px;
+        margin-top: 10px;
     }
+
+    .secondRow {
+        background-color: lightgrey;
+    }
+
+   
 
     .jobstart {
         flex: 1;
